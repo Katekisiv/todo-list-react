db989cab6cb46e961af715c4b7967372c148139e
Kate Kisiv <katerinakisiv@gmail.com>
2022-05-17 18:14:52 +0300

    fix: updating tsconfig.json feat: add layout for todo app, working on fetching and rendering todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
-import './App.css'
-import Hi from './Hi'
+import  './normalize.css'
+import  './App.css'
+//import LoginPage from "./pages/LoginPage/LoginPage";
+import TodoPage from "./pages/TodoPage/TodoPage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Hello, World!</h1>
-        <Hi />
-      </div>
+      <TodoPage/>
     )
   }
 }
