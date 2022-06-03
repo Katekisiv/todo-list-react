2c64f43b3df50a0546bcbaebe6102e46f4737517
Kate Kisiv <katerinakisiv@gmail.com>
2022-06-03 16:11:21 +0300

    fix: different validationSchema for login and registration pages, fix validation for matching passwords, validate all fields simultaneously

--- todo-list-react/src/constants/validationSchema.ts
+++ todo-list-react/src/constants/validationSchema.ts
@@ -1,20 +1,47 @@
 import { object, string, date, ref } from 'yup'
 
-export const validationSchema = object().shape({
-  name: string()
+import { parse, isDate } from 'date-fns'
+
+function parseDateString(_: any, originalValue: any) {
+  return isDate(originalValue)
+    ? originalValue
+    : parse(originalValue, 'yyyy-MM-dd', new Date())
+}
+
+export const registrationValidationSchema = object({
+  firstName: string()
+    .required('Name is required')
     .trim()
     .min(1, 'Must be at least 1 symbols')
-    .max(12, 'Must be no more than 12 symbols')
-    .required('Name is required'),
+    .max(12, 'Must be no more than 12 symbols'),
+  lastName: string()
+    .required('Name is required')
+    .trim()
+    .min(1, 'Must be at least 1 symbols')
+    .max(12, 'Must be no more than 12 symbols'),
   email: string().trim().email('Invalid email').required('Email is required'),
   password: string()
     .trim()
     .min(8, 'Must be at least 8 symbols')
     .required('Password is required'),
-  confirmPassword: string().when('password', (password, field) =>
-    password ? field.required().oneOf([ref('password')]) : field
-  ),
+  confirmPassword: string()
+    .required('Confirm password is required')
+    .when('password', (password, field) =>
+      password
+        ? field.required().oneOf([ref('password')], 'Passwords do not match')
+        : field
+    ),
+  sex: string().oneOf(['male', 'female', 'others']),
   birthday: date()
+    .transform(parseDateString)
     .min(new Date(Date.now() - 3905104000999), 'Invalid Date')
     .max(new Date(), 'Invalid Date'),
+})
+
+export const loginValidationSchema = object({
+  email: string().trim().email('Invalid email').required('Email is required'),
+  password: string()
+    .trim()
+    .min(8, 'Must be at least 8 symbols')
+    .required('Password is required'),
 })
